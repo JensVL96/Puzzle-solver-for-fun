923741c052d27e45fa97af92e081045599067b77
Jens <jle040@uit.no>
2020-10-31 02:56:06 +0100

    split the file for an easier read, added block highlight/ input

--- sudoku.py
+++ sudoku.py
@@ -1,216 +1,63 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import pygame as pg
-from random import sample
 from config import *
+from create_board import *
+from solve_bloard import *
+from display_board import *
+
 from string import *
+import pygame as pg
 import numpy as np
 
+# For error highlighting
+def set_highlight(row, col, blk, lock):
+    global input_lock
+    input_lock = lock
+    global row_index
+    row_index = row
+    global col_index
+    col_index = blk
+    global blk_index
+    blk_index = col
+
 def get_cord(pos): 
-    global x 
-    x = pos[0]//BLOCK_SIZE 
-    global y 
-    y = pos[1]//BLOCK_SIZE 
-
-class create_board():
-    def __init__(self):
-        self.base  = 3  # Will generate any size of random sudoku board in O(n^2) time
-        self.side  = self.base * self.base
-        self.nums  = sample(range(1, self.side + 1), self.side) # random numbers
-        self.board = [[self.nums[(self.base * (r%self.base) + r//self.base + c)%self.side] for c in range(self.side) ] for r in range(self.side)]
-        rows  = [ r for g in sample(range(self.base),self.base) for r in sample(range(g * self.base,(g + 1) * self.base), self.base) ] 
-        cols  = [ c for g in sample(range(self.base),self.base) for c in sample(range(g * self.base,(g + 1) * self.base), self.base) ]            
-        self.board = [[self.board[r][c] for c in cols] for r in rows]
-
-        # print("\nInput:")
-        # for line in self.board: print(line)
-
-        squares = self.side * self.side
-        empties = squares * 3//4
-        for p in sample(range(squares),empties):
-            self.board[p//self.side][p%self.side] = 0
-
-        self.lines()
-
-    def expandLine(self, line):
-        return line[0]+line[5:9].join([line[1:5]*(self.base-1)]*self.base)+line[9:13]
-        
-    def lines(self):
-        self.line0  = self.expandLine("╔═══╤═══╦═══╗")
-        self.line1  = self.expandLine("║ . │ . ║ . ║")
-        self.line2  = self.expandLine("╟───┼───╫───╢")
-        self.line3  = self.expandLine("╠═══╪═══╬═══╣")
-        self.line4  = self.expandLine("╚═══╧═══╩═══╝")
-
-        self.draw()
-
-    def draw(self):
-        symbol = " 1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        self.nums   = [ [""]+[symbol[n] for n in row] for row in self.board ]
-        print(self.line0)
-        for r in range(1,self.side+1):
-            print( "".join(n+s for n,s in zip(self.nums[r-1],self.line1.split("."))) )
-            print([self.line2,self.line3,self.line4][(r%self.side==0)+(r%self.base==0)])
-
-class solve_board():
-    def __init__(self, board):
-        self.row = []
-        self.col = []
-        self.cell = []
-        self.row_list = []
-        self.col_list = []
-        self.cell_list = []
-
-        for space in range(9):
-            self.col.append([])
-            self.cell.append([])
-
-        row_idx = 0
-        for line in board:
-
-            self.row.append(line)
-
-            cell_idx = 0
-            if row_idx >= 3:
-                cell_idx = 3
-            if row_idx >= 6:
-                cell_idx = 6
-
-            
-            for col_idx in range(9):
-
-                self.col[col_idx].insert(row_idx, line[col_idx])
-
-                if col_idx % 3 == 0:
-                    for triple in range(0, 3):
-                        self.cell[cell_idx].insert(len(self.cell[row_idx]) + triple, line[col_idx + triple])
-                    cell_idx += 1
-
-            self.row_list.append(self.row)
-            self.col_list.append(self.col)
-            self.cell_list.append(self.cell)
-            row_idx += 1
-
-        print("\nrow:")
-        for row in self.row_list[0]:
-            print(row)
-        # print("\ncolumn:")
-        # for col in self.col_list[0]:
-        #     print(col)
-        # print("\ncell:")
-        # for cell in self.cell_list[0]:
-        #     print(cell)
-
-    def assign_flags(self, board):
-        self.flags = []
-        row_idx = 0
-        cell_idx = 0
-
-        print("\n")
-        for line in board:
-            cell_idx = 0
-            if row_idx >= 3:
-                cell_idx = 3
-            if row_idx >= 6:
-                cell_idx = 6
-
-            for index in range(9):
-                # print("position: ", index, "value: ", line[index])
-
-                # print("row", row_idx, "col", index, "cell", cell_idx)
-                if (index % 3 == 0 and index != 0):
-                    cell_idx += 1
-
-                if line[index] == 0:
-                    flag_idx = 0
-                    temp_flag = []
-                    for value in range(1, 10):
-                        # print(value)
-                        if self.row_flag(value, row_idx):
-                            # print("found in row")
-                            pass
-                        elif self.col_flag(value, index):
-                            # print("found in column")
-                            pass
-                        elif self.cell_flag(value, cell_idx):
-                            # print("found in cell")
-                            pass
-                        else:
-                            temp_flag.append(value)
-                            flag_idx += 1
-                    print(temp_flag)
-                    self.flags.append(temp_flag)
-            row_idx += 1
-
-    def check_row(self):
-        pass
-
-    def column(self, x):
-        pass
-
-    def cell(self, row, col):
-        pass
-
-    def row_flag(self, index, row_idx):
-        for row in self.row_list[0][row_idx]:
-            # print("comparing in row ", row, "with ", index, "row_idx ", row_idx)
-            if row == index:
-                return 1
-        return 0
-    
-    def col_flag(self, index, col_idx):
-        for col in self.col_list[0][col_idx]:
-            # print("comparing in column ", col, "with ", index, "col_idx ", col_idx)
-            if col == index:
-                return 1
-        return 0
-
-    def cell_flag(self, index, cell_idx):
-        for cell in self.cell_list[0][cell_idx]:
-            # print("comparing in cell ", cell, "with ", index, "cell_idx ", cell_idx)
-            if cell == index:
-                return 1
-        return 0
+    global box_index_x 
+    box_index_x = (pos[0] - TOP_LX)//BLOCK_SIZE 
+    global box_index_y 
+    box_index_y = (pos[1] - TOP_LY)//BLOCK_SIZE 
 
 
-class Display_board():
-    def __init__(self, screen):
-        self.screen = screen
-        self.font1 = pg.font.SysFont("comicsans", 40) 
-        
+def valid(grid, x, y, val, increase): 
+    input_lock = 0
+    row = col = blk = (0, 0)
 
-    def draw_val(self, val):
-        text1 = self.font1.render(str(val), 1, BLACK)
-        self.screen.blit(text1, (x * BLOCK_SIZE + 15, y * BLOCK_SIZE + 15))
+    for index in range(9): 
+        # Check if value in column
+        if grid[x][index] == val: 
+            col = (x, index)
+            input_lock = 1
 
-    def on_mouse_press(self, x, y, symbol, modifier):
-        pass
+        # Check if value in row
+        if grid[index][y] == val: 
+            row = (index, y)
+            input_lock = 1
 
-    def draw(self, grid):
-        for i in range (9):
-            for j in range (9):
-                if grid[i][j] != 0:
+    # Finds the block
+    index_x = x // 3 # integer division
+    index_y = y // 3
 
-                    text1 = self.font1.render(str(grid[i][j]), 1, BLACK)
-                    self.screen.blit(text1, (TOP_LX + i * BLOCK_SIZE + 15, TOP_LY + j * BLOCK_SIZE + 15))
+    # Check if value in block
+    for i in range(index_x * 3, index_x * 3 + 3): 
+        for j in range (index_y * 3, index_y * 3 + 3): 
+            if grid[i][j] == val: 
+                blk = (i, j)
+                input_lock = 1
 
-        for i in range(10):
-            if i %3 == 0:
-                thick = 7
-            else:
-                thick = 1
-            pg.draw.line(self.screen, BLACK, (TOP_LX, 
-                                            TOP_LY + i * BLOCK_SIZE), 
-                                            (TOP_RX, 
-                                            TOP_RY + i * BLOCK_SIZE), thick)
-            pg.draw.line(self.screen, BLACK, (TOP_LX + i * BLOCK_SIZE, 
-                                            TOP_LY), 
-                                            (BOT_LX + i * BLOCK_SIZE, 
-                                            BOT_LY), thick)
+    if input_lock == 1:
+        set_highlight(row, col, blk, input_lock)
+        return False
 
-    def update(self, dt):
-        pass
-
+    return True
 
 class Main():
     def __init__(self):
@@ -226,8 +73,14 @@ class Main():
 
         flag1 = 0
         val = 0
+        pos = (0, 0)
+        
+        input_lock = 0
+        get_cord((0, 0))
+        set_highlight((0, 0), (0, 0), (0, 0), input_lock)
 
-        self.board = create_board().board
+        board = create_board().board
+
         while 1:
             for event in pg.event.get():
                 if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
@@ -236,7 +89,7 @@ class Main():
                     flag1 = 1
                     pos = pg.mouse.get_pos()
                     get_cord(pos)
-                if event.type == pg.KEYDOWN:
+                if event.type == pg.KEYDOWN and input_lock != 1:
                     if event.key == pg.K_1:
                         val = 1
                     if event.key == pg.K_2:
@@ -255,29 +108,38 @@ class Main():
                         val = 8
                     if event.key == pg.K_9:
                         val = 9
+                elif event.type == pg.KEYDOWN and input_lock == 1:
+                    if event.key == pg.K_BACKSPACE:
+                        val = 0
+                        set_highlight((0, 0), (0, 0), (0, 0), 0)
 
             if val != 0:
-                display.draw_val(val)
+                display.draw_val(val, box_index_x, box_index_y)
 
-                if valid(grid, int(x), int(y), val) == True:
-                    grid[int(x)][int(y)] = val
+                if valid(board, int(box_index_x), int(box_index_y), val, display):
+                    board[int(box_index_x)][int(box_index_y)] = val
                 else:
-                    grid[int(x)][int(y)] = 0
-                    raise_error2()
+                    board[int(box_index_x)][int(box_index_y)] = 0
                 val = 0
 
             pg.draw.rect(self.screen, BLACK, (0, 0, self.screen.get_width(), self.screen.get_height()))
             self.screen.fill(BEIGE)
 
-            display.draw(self.board)
+            display.draw(board)
+            #display.glow(box_index_x, box_index_y)
+            print(box_index_x, box_index_y)
+
+            if input_lock == 1:
+                display.update(board, row_index, col_index, blk_index)
+
             # display.draw_box()
 
             pg.display.update()
 
 
-        self.solution = solve_board(self.board)
+        self.solution = solve_board(board)
 
-        self.solution.assign_flags(self.board)
+        self.solution.assign_flags(board)
 
 if __name__ == '__main__':
     Main()
